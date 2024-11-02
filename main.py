f1ce29d1fc73e07ef603d01dbdb48f698ad50b05
darrenxx3 <cdarren200503@gmail.com>
2024-11-02 20:07:03 +0700

    check commit dari vs code ke streamlit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 tabel =pd.DataFrame({"Anime": ["Kanojo Okarishimasu","Naruto","Bleach","Frieren","Cyberpunk Edgerunners","Roshidere"], "Category": [11,12,13,14,15,16]})
 
-st.title("Hello World, I'm Darren")
+st.title("Hello World, I'm Darren a weeb")
 st.subheader("This is streamlit testing")
 st.header("Testing")
 st.text("Streamlit text")
@@ -22,3 +22,6 @@ st.code(code, language="python")
 st.write("# h1")
 st.metric(label="Wind Speed", value="190ms-1", delta="-1.4ms\^-1")
 st.table(tabel)
+st.dataframe(tabel)
+
+#dataframe and table are not the same
